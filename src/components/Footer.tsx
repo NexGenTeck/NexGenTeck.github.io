b8b4835766981a4403f8c1aab29d7aadf0727073
NexGenTeck <nexgenteckcom@gmail.com>
2026-06-29 00:58:43 +0500

    Fix footer social width style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -63,7 +63,7 @@ export const Footer: React.FC = () => {
               <div
                 className="flex items-center justify-between"
                 style={{
-                  width: `${footerSocialWidth}2.5px`,
+                  width: `${footerSocialWidth}1px`,
                 }}
               >
                 <a
