64a3c88b9e4a4a0f4c67e812aefda9068afec32d
NexGenTeck <nexgenteckcom@gmail.com>
2026-07-14 00:07:55 +0500

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -720,7 +720,7 @@ export const Home: React.FC = () => {
                 <iframe
                   className="absolute top-0 left-0 w-full h-full"
                   style={{ zIndex: 1 }}
-                  src="https://www.youtube.com/embed/6lRpZ2fFdoI?si=HAufigQmWWwfGHZE?autoplay=1&rel=0"
+                  src="https://www.youtube.com/embed/6lRpZ2fFdoI?autoplay=1&rel=0"
                   title="NexGenTeck Quick Demo"
                   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                   referrerPolicy="strict-origin-when-cross-origin"
