e9c9f93dde0009ef1500e62575dae57d0b11eed9
muhammadhasaan82 <muhammadhasaan82@users.noreply.github.com>
2026-09-20 08:58:28 +0000

    deploy: c22cc5e301a9a1ccd8696c961f260a35e80bb0d0

---
+++ contact-config.example.php
@@ -0,0 +1,15 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'database');
+define('DB_USER', 'username');
+define('DB_PASSWORD', 'password');
+
+define('SMTP_ENABLED', false);
+define('SMTP_HOST', 'smtp.hostinger.com');
+define('SMTP_PORT', 465);
+define('SMTP_USERNAME', 'username@example.com');
+define('SMTP_PASSWORD', 'password');
+define('SMTP_FROM_EMAIL', 'noreply@example.com');
+define('SMTP_FROM_NAME', 'NexGenTeck Website');
+define('SMTP_TO_EMAIL', 'info@nexgenteck.com');
